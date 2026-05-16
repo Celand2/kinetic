ec044673a94d5a1205b94fa718942f9409296162
Celand2 <ndikuceland@gmail.com>
2026-05-16 13:02:25 +0300

    icone PWA'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @laravelPWA
     <title>@yield('title', 'KTS') — Kinetic Trading System</title>
 
     @vite(['resources/css/app.css','resources/css/style.css','resources/js/app.js'])
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    build: {
+    manifest: 'manifest.json', // force le manifest à la racine du build
+},
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
